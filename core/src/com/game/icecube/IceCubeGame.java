1da2bc88eb43212c28d0a9de48e6e43fda2c8336
Freris-ryan <freris.ryan@gmail.com>
2015-06-14 22:56:09 -0400

    Update IceCubeGame.java

--- core/src/com/game/icecube/IceCubeGame.java
+++ core/src/com/game/icecube/IceCubeGame.java
@@ -14,11 +14,6 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 
 import sun.applet.Main;
 
-/**
- * Main Ice Cube class which creates and renders the game
- * starting from the main menu
- **/
-
 public class IceCubeGame extends Game {
     Game game;
 	@Override
